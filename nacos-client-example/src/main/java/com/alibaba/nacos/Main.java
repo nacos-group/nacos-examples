6e8ba03a0e6df52a86cfec6ed764c4655d1c81cb
yanlinly <yan.lin2009@163.com>
2022-06-16 20:04:41 +0800

    update nacos-client config demo

---
+++ nacos-client-example/src/main/java/com/alibaba/nacos/Main.java
@@ -0,0 +1,25 @@
+package com.alibaba.nacos;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String serverAddr = "127.0.0.1:8848";
+            String dataId = "test";
+            String group = "test";
+            Properties properties = new Properties();
+            properties.put("serverAddr", serverAddr);
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            String content = configService.getConfig(dataId, group, 5000);
+            System.out.println(content);
+        } catch (NacosException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
